399deba963c6a5c04f266303b99f93111fdc6810
Yashodhan <yashodhanvashisht@gmail.com>
2023-02-15 19:24:06 +0530

    added moment, semantic UI, styled the app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//developer imports
+import 'semantic-ui-css/semantic.min.css'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
